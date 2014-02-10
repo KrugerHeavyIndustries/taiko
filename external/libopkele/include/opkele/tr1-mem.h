1305645ff234c2b0fcec6ec2b0efa5ff85572d9a
Chris Kruger <chris.kruger@agworld.com.au>
2014-02-10 22:12:31 +0800

    added stamp-h2 and tr1-mem.h

---
+++ external/libopkele/include/opkele/tr1-mem.h
@@ -0,0 +1,14 @@
+#ifndef __OPKELE_TR1_MEM_H
+#define __OPKELE_TR1_MEM_H
+
+#if defined(__APPLE__)
+#include <memory>
+#else
+#include <tr1/memory>
+#endif
+
+//namespace opkele {
+ //   namespace tr1mem = std::tr1;
+//}
+
+#endif /* __OPKELE_TR1_MEM_H */
