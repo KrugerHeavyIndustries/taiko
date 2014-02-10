91e54e8d84b6cb85df482d197dbfe47de8619541
Chris Kruger <chris.kruger@agworld.com.au>
2014-02-10 22:09:40 +0800

    acconfig.h file add - ignored previously

---
+++ external/libopkele/include/opkele/acconfig.h
@@ -0,0 +1,8 @@
+/* include/opkele/acconfig.h.  Generated from acconfig.h.in by configure.  */
+
+/* defined in presence of konforka library */
+/* #undef OPKELE_HAVE_KONFORKA */
+
+/* deprecated function attribute */
+#define OPKELE_DEPRECATE __attribute__((deprecated))
+
