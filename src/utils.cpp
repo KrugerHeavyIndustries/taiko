72020c1b81f5f3e031221ed7a41fae97199d318a
montdidier <chris.kruger@krugerheavyindustries.com>
2014-11-11 12:34:04 +0800

    added license and header information to source

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,3 +1,41 @@
+/*
+ *	__  __ ______ _______ _______ _______ ______ 
+ *	|  |/  |   __ \   |   |     __|    ___|   __ \
+ *	|     <|      <   |   |    |  |    ___|      <
+ *	|__|\__|___|__|_______|_______|_______|___|__|
+ *	       H E A V Y  I N D U S T R I E S
+ *
+ *	Copyright (C) 2014 Krüger Heavy Industries
+ *	http://www.krugerheavyindustries.com
+ *
+ * Written by Chris Kruger AT krugerheavyindustries.com
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 #include "utils.h"
 
 #include <string>
